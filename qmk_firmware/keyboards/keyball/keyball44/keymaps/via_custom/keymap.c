b8affda852dfa895310ab783f3744b8f7ba09645
R <46211472+Req1630@users.noreply.github.com>
2025-10-21 21:45:24 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/keymap.c
@@ -69,6 +69,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 #ifdef OLED_ENABLE
 
 #include "lib/oledkit/oledkit.h"
+#include "oled_custom.c"
 
 void oledkit_render_info_user(void) {
     keyball_oled_render_keyinfo();
